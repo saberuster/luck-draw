555abac446644ee7dba593b0c9bda0c67dd52eaa
saberuster <saberuster@gmail.com>
2018-09-16 09:48:52 +0800

    use default rand with rand.Seed

--- rand.go
+++ rand.go
@@ -7,10 +7,12 @@ import (
 )
 
 var (
-	r = rand.New(rand.NewSource(time.Now().UnixNano()))
-
 	endLessThanCountErr = errors.New("luck-draw:rand num:end can not less than count")
 )
+
+func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
+}
 
 func randNum(end, count int) (result []int, err error) {
 	if end < count {
@@ -18,7 +20,7 @@ func randNum(end, count int) (result []int, err error) {
 	}
 	m := make(map[int]interface{}, count)
 	for len(m) < count {
-		m[r.Intn(end)] = nil
+		m[rand.Intn(end)] = nil
 	}
 
 	for k := range m {
